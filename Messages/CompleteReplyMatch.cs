8828d4939ec6bce58d8f75d6e725c69058bf828a
Mostafa Hagras <mhagras@hotmail.co.uk>
2018-05-27 09:32:47 +0100

    Added and Accomodated New Match Type

---
+++ Messages/CompleteReplyMatch.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace iCSharp.Messages
+{
+    public class CompleteReplyMatch
+    {
+
+          [JsonProperty("name")]
+          public string Name { get; set; }
+
+          [JsonProperty("documentation")]
+          public string Documentation { get; set; }
+
+          [JsonProperty("value")]
+          public string Value { get; set; }
+
+
+      
+    }
+}
