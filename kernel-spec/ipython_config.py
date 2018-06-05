2190190973ebe54a5fa75c1c6b6ec5a8c2c07959
Omer <omerouz@gmail.com>
2018-06-05 23:49:02 +0100

    ipython_config platform specific

--- kernel-spec/ipython_config.py
+++ kernel-spec/ipython_config.py
@@ -1,3 +1,15 @@
+from platform import system
+from os.path import expanduser
+
+if system() == "Windows":
+	path = "C:\\ProgramData\\jupyter\\kernels\\csharp\\static"
+elif system() == "Darwin":
+	path = expanduser("~") + "/Library/jupyter/kernels/csharp/static"
+elif system() == "Linux":
+	path = expanduser("~") + "/.local/share/jupyter/kernels/csharp/static"
+else:
+	raise Exception("Unknown OS")
+
 c = get_config()
-c.NotebookApp.extra_static_paths = [ r"C:\ProgramData\jupyter\kernels\csharp\static" ]
+c.NotebookApp.extra_static_paths = [ path ]
 c.update('notebook', {"CodeCell": {"cm_config": {"autoCloseBrackets": False}}})
